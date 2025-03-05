a328720d04de58a265a686113d510d2821cae197
SavedByLight <mrfluffyoven@gmail.com>
2025-03-05 07:46:38 +0000

    adapt mt6835 common tree

--- twrp_a16xm.mk
+++ twrp_a16xm.mk
@@ -1,16 +1,5 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Release name
+PRODUCT_RELEASE_NAME := A16
 
 # Inherit from a16xm device
 $(call inherit-product, device/samsung/a16xm/device.mk)
@@ -21,9 +10,4 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A166P
 PRODUCT_MANUFACTURER := samsung
 
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="a16xmnsxx-user 13 TP1A.220624.014 A166PXXU1AXIC release-keys"
-
-BUILD_FINGERPRINT := samsung/a16xmnsxx/a16xm:13/TP1A.220624.014/A166PXXU1AXIC:user/release-keys
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a16xm/recovery/root,recovery/root)
